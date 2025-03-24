4505d128ebd05090a0288acc9171e11a5c8703a6
DangQuangDat <june23second002@gmail.com>
2025-03-24 18:09:22 +0700

    Day02 answer questions

--- src/ex_day2/Generics_Decorators.ts
+++ src/ex_day2/Generics_Decorators.ts
@@ -1,7 +1,13 @@
-enum Direction {
-    North,
-    East,
-    South,
-    West
-  }
-console.log(Direction[1])
+Người báo cáo: Đặng Quang Đạt
+Task dự kiến:
+Trả lời câu hỏi Day1
+Thực hành training Day1
+Thực hành bài training Day2
+Trả lời câu hỏi Day2
+Task đã làm
+Làm thực hành bài training Day1: 100%
+Trả lời câu hỏi Day1: 100%
+Trả lời câu hỏi Day2: 100%
+Làm thực hành bài training Day2: 0%
+Khó khăn:
+Không có
