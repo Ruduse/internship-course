bf738cadb43232c09f2c5238fb019ed971c84b57
DangQuangDat <june23second002@gmail.com>
2025-03-26 15:21:37 +0700

    day4 update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
-import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
+// import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+// import { UsersModule } from './users/users.module';
+// import { LoggerMiddleware } from './middleware/logger.middleware';
+// @Module({
+//   imports: [UsersModule],
+// })
+// export class AppModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer.apply(LoggerMiddleware).forRoutes({
+//       path: 'users',
+//       method: RequestMethod.ALL,
+//     });
+//   }
+// }
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './users/module/user.module';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs'), // Kết nối MongoDB
+    UserModule,
+  ],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes({
-      path: 'users',
-      method: RequestMethod.ALL,
-    });
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,21 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import mongoose from 'mongoose';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+//   console.log('🚀 Server đang chạy tại http://localhost:3000');
+
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/nestjs');
+//     console.log('✅ Kết nối MongoDB thành công!');
+//   } catch (error) {
+//     console.error('❌ Lỗi kết nối MongoDB:', error);
+//   }
+// }
+// bootstrap();
